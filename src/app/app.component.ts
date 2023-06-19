f52bde31875c84e7aae81618aa38eed96f7c6932
DorianRONDI <dorianrondi@gmail.com>
2023-06-19 13:07:58 +0200

    Afficher des liste avec *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,31 @@ import { FaceSnap } from './models/face-snap.model';
 
 export class AppComponent implements OnInit{
   'title' = 'BeginAngluar';
-  snap1!: FaceSnap;
-  snap2!: FaceSnap;
-  snap3!: FaceSnap;
+  snaps!: FaceSnap[];
 
   ngOnInit() {
-    this.snap1 = {
-      title :'Archibald',
-      description : 'Mon meilleur ami depuis tout petit !',
-      created_At : new Date(),
-      snaps : 0,
-      imageURL : 'https://source.unsplash.com/random/100×100/?lingerie'
-    }
-
-    this.snap2 = {
-      title :'Archibald',
-      description : 'Mon meilleur ami depuis tout petit !',
-      created_At : new Date(),
-      snaps : 0,
-      imageURL : 'https://source.unsplash.com/random/100×100/?dress'
-    }
-
-    this.snap3 = {
-      'title' :'Archibald',
-      description : 'Mon meilleur ami depuis tout petit !',
-      created_At : new Date(),
-      snaps : 0,
-      imageURL : 'https://source.unsplash.com/random/100×100/?snowboard',
-      'location' : 'Alpes'
-    }
+    this.snaps = [
+      {
+        title :'Lingerie',
+        description : "C'est sexy !",
+        created_At : new Date(),
+        snaps : 6,
+        imageURL : 'https://source.unsplash.com/random/100×100/?lingerie'
+      },{
+        title :'Robe',
+        description : "C'est jolie !",
+        created_At : new Date(),
+        snaps : 0,
+        imageURL : 'https://source.unsplash.com/random/100×100/?dress'
+      },{
+        'title' :'Snowboard',
+        description : "C'est funky !",
+        created_At : new Date(),
+        snaps : 3,
+        imageURL : 'https://source.unsplash.com/random/100×100/?snowboard',
+        'location' : 'Alpes'
+      }
+    ];
   }
 }
 
